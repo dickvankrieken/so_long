f83db7f890aed3e281188032b59fac1d7b8da3fa
dirkvankrieken <45898176+dirkvankrieken@users.noreply.github.com>
2021-10-21 16:10:59 +0200

    update

--- srcs/map.c
+++ srcs/map.c
@@ -6,28 +6,21 @@
 /*   By: dvan-kri <dvan-kri@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/20 12:37:46 by dvan-kri      #+#    #+#                 */
-/*   Updated: 2021/10/20 12:37:48 by dvan-kri      ########   odam.nl         */
+/*   Updated: 2021/10/21 13:04:37 by dvan-kri      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <mlx.h>
 #include <fcntl.h>
+#include <stdio.h>
 #include "../includes/so_long.h"
 #include "../libft/gnl/get_next_line.h"
 #include "../libft/includes/ft_printf.h"
 
-static void	game_vars_init(t_game *game)
-{
-	game->map.exit = 0;
-	game->map.collectibles = 0;
-	game->map.player = 0;
-	game->player.moves = 0;
-}
-
 static void	put_images_set_player_coordinates(t_game *d, int i, int j)
 {
 	mlx_put_image_to_window(d->window.mlx, d->window.win,
-		d->img.img, j * 64, i * 64);
+		d->g_img.img, j * 64, i * 64);
 	if (d->map.data[i][j] == 'P')
 	{
 		mlx_put_image_to_window(d->window.mlx, d->window.win,
@@ -46,7 +39,7 @@ static void	put_images_set_player_coordinates(t_game *d, int i, int j)
 			d->e_img.img, j * 64, i * 64);
 }
 
-void	validate_map_dimensions(t_game *game, char *argv_map)
+void	load_map_dimensions(t_game *game, char *argv_map)
 {
 	char	*line;
 	int		ret;
@@ -54,23 +47,18 @@ void	validate_map_dimensions(t_game *game, char *argv_map)
 
 	game_vars_init(game);
 	fd = open(argv_map, O_RDONLY);
-	ret = get_next_line(fd, &line);
-	line_is_walls(line);
-	game->map.columns = ft_strlen(line);
-	game->map.rows = 1;
+	if (fd == -1)
+	{
+		ft_printf("Error\nFailed to open map file");
+		exit(EXIT_FAILURE);
+	}
+	ret = 1;
 	while (ret > 0)
 	{
 		ret = get_next_line(fd, &line);
-		check_tiles(game, line);
-		if (ft_strlen(line) != game->map.columns)
-		{
-			ft_printf("Error, map is not rectangular\n");
-			exit(EXIT_FAILURE);
-		}
 		game->map.rows++;
+		free(line);
 	}
-	check_requirements(game);
-	line_is_walls(line);
 }
 
 void	free_map_data(t_map *map, int number)
@@ -93,10 +81,6 @@ void	draw_map(t_game *d)
 
 	i = 0;
 	j = 0;
-	d->img.img = mlx_xpm_file_to_image(d->window.mlx, "./assets/grass.xpm",
-			&d->img.width, &d->img.height);
-	d->img.addr = mlx_get_data_addr(d->img.img, &d->img.bpp,
-			&d->img.line, &d->img.endian);
 	while (i < d->map.rows)
 	{
 		while (j < d->map.columns)
