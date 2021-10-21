928f10d0edede69880fc4a3e61f34071ddb8a317
dirkvankrieken <45898176+dirkvankrieken@users.noreply.github.com>
2021-10-21 16:10:36 +0200

    added grass tile to game_init

--- srcs/game.c
+++ srcs/game.c
@@ -6,7 +6,7 @@
 /*   By: dvan-kri <dvan-kri@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/20 12:32:35 by dvan-kri      #+#    #+#                 */
-/*   Updated: 2021/10/20 12:32:37 by dvan-kri      ########   odam.nl         */
+/*   Updated: 2021/10/21 13:05:53 by dvan-kri      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,23 @@
 #include "../includes/keycodes.h"
 #include "../libft/includes/ft_printf.h"
 
+void	game_vars_init(t_game *game)
+{
+	game->map.exit = 0;
+	game->map.collectibles = 0;
+	game->map.player = 0;
+	game->map.rows = 0;
+	game->map.columns = 0;
+	game->player.moves = 0;
+}
+
 void	game_init(t_game *d)
 {
 	d->window.mlx = mlx_init();
+	d->g_img.img = mlx_xpm_file_to_image(d->window.mlx, "./assets/grass.xpm",
+			&d->g_img.width, &d->g_img.height);
+	d->g_img.addr = mlx_get_data_addr(d->g_img.img, &d->g_img.bpp,
+			&d->g_img.line, &d->g_img.endian);
 	d->p_img.img = mlx_xpm_file_to_image(d->window.mlx, "./assets/player.xpm",
 			&d->p_img.width, &d->p_img.height);
 	d->p_img.addr = mlx_get_data_addr(d->p_img.img, &d->p_img.bpp,
