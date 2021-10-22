bf67705ac3cc0d86d7c1bf1608f608ce45b40493
dirkvankrieken <45898176+dirkvankrieken@users.noreply.github.com>
2021-10-22 16:55:08 +0200

    comply to norme

--- srcs/map_check.c
+++ srcs/map_check.c
@@ -6,31 +6,36 @@
 /*   By: dvan-kri <dvan-kri@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/20 12:37:53 by dvan-kri      #+#    #+#                 */
-/*   Updated: 2021/10/21 18:15:25 by dvan-kri      ########   odam.nl         */
+/*   Updated: 2021/10/22 16:22:55 by dvan-kri      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fcntl.h>
 #include <stdlib.h>
 #include "../includes/so_long.h"
 #include "../libft/includes/ft_printf.h"
+
+static void	free_and_exit(t_map map)
+{
+	free_map_data(&map, map.rows);
+	exit(EXIT_FAILURE);
+}
 
 void	check_requirements(t_map map)
 {
 	if (map.exit == 0)
 	{
 		ft_printf("Error\nInvalid map, must contain an exit\n");
-		exit(EXIT_FAILURE);
+		free_and_exit(map);
 	}
 	if (map.collectibles == 0)
 	{
 		ft_printf("Error\nInvalid map, must contain collectibles\n");
-		exit(EXIT_FAILURE);
+		free_and_exit(map);
 	}
 	if (map.player == 0)
 	{
 		ft_printf("Error\nInvalid map, there must be one player\n");
-		exit(EXIT_FAILURE);
+		free_and_exit(map);
 	}
 }
 
@@ -38,17 +43,15 @@ void	check_tiles(t_game *game, char *l)
 {
 	if (*l != '1')
 	{
-		/* er staat opeens niets meer op de plek waar de 0e pointer van de map.data array naar wijst */
-		ft_printf("%s??", game->map.data[0]);
 		ft_printf("Error\nWrong map format, not surrounded by only walls\n");
-		exit(EXIT_FAILURE);
+		free_and_exit(game->map);
 	}
 	while (*l != '\0')
 	{
 		if (!(*l == '1' || *l == '0' || *l == 'C' || *l == 'E' || *l == 'P'))
 		{
 			ft_printf("Error\nInvalid map, contains invalid characters\n");
-			exit(EXIT_FAILURE);
+			free_and_exit(game->map);
 		}
 		if (*l == 'E')
 			game->map.exit++;
@@ -61,21 +64,18 @@ void	check_tiles(t_game *game, char *l)
 	if (*(l - 1) != '1')
 	{
 		ft_printf("Error\nWrong map format, not surrounded by only walls\n");
-		exit(EXIT_FAILURE);
+		free_and_exit(game->map);
 	}
 }
 
 void	line_is_walls(t_map map, char *line)
 {
-
 	while (*line != '\0')
 	{
 		if (*line != '1')
 		{
 			ft_printf("Error\nWrong map format, not surrounded by only walls\n");
-//			free_map_data(&map, number);
-//			while (1);
-			exit(EXIT_FAILURE);
+			free_and_exit(map);
 		}
 		line++;
 	}
